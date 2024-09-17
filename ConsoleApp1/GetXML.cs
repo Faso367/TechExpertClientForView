461547532c6dd9f78d22a8d0fb74a9227eb38532
John White <pupkov.54@inbox.ru>
2024-09-17 17:41:31 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/GetXML.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class MessageInspector : IClientMessageInspector
+    {
+        public void AfterReceiveReply(ref Message reply, object correlationState)
+        {
+            //Получение сырого XML - ответа
+            string rawXml = reply.ToString();
+            Console.WriteLine("Сырой XML-ответ:");
+            Console.WriteLine(rawXml);
+        }
+
+        public object BeforeSendRequest(ref Message request, IClientChannel channel)
+        {
+            // Здесь можно логировать исходящее сообщение (запрос)
+            string rawXmlRequest = request.ToString();
+            Console.WriteLine("Исходящий SOAP-запрос:");
+            Console.WriteLine(rawXmlRequest);
+
+            return null;
+        }
+    }
+
+    public class CustomBehavior : IEndpointBehavior
+    {
+        public void AddBindingParameters(ServiceEndpoint endpoint, BindingParameterCollection bindingParameters) { }
+
+        public void ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime)
+        {
+            clientRuntime.ClientMessageInspectors.Add(new MessageInspector());
+        }
+
+        public void ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher) { }
+
+        public void Validate(ServiceEndpoint endpoint) { }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TechExpertRef;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+
+    class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            string text = "гражданский кодекс";
+            string url2 = "http://192.168.0.14:81/kodeks/bparser?parse=" + Uri.EscapeDataString(text);
+            string url = "http://192.168.0.14:81/docs/api";
+
+
+            using (HttpClient httpclient = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await httpclient.GetAsync(url2);
+                    response.EnsureSuccessStatusCode();
+                    string bparserResult = await response.Content.ReadAsStringAsync();
+
+                    var client2 = new apiSoapClient();
+                    client2.Endpoint.EndpointBehaviors.Add(new CustomBehavior());
+
+                    var resz = client2.FuzzySearch("гражданский кодекс", null, 0, "searchbynames", bparserResult);
+
+                    var req3 = new GetDocsInfoRequest("901714421", "1");
+                    Console.WriteLine();
+                    var resp3 = client2.GetDocsInfo(req3);
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine($"Ошибка запроса: {e.Message}");
+                }
+            }
+        }
+    }
+
+}
+
